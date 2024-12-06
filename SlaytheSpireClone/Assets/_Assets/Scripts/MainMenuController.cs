f4db39d23be9767daf80c876de0a3f0de3af7eae
kalste <kal9107@naver.com>
2024-12-07 00:00:56 +0900

    Fix line endings

--- SlaytheSpireClone/Assets/_Assets/Scripts/MainMenuController.cs
+++ SlaytheSpireClone/Assets/_Assets/Scripts/MainMenuController.cs
@@ -3,8 +3,12 @@ using UnityEngine.UIElements;
 
 public class MainMenuController : MonoBehaviour
 {
-   private Button startButton;
-    private Button optionsButton;
+    private Button continueButton;
+    private Button quitBattleButton;
+    private Button encyclopediaButton;
+    private Button statisticsButton;
+    private Button settingsButton;
+    private Button patchNotesButton;
     private Button quitButton;
 
     void OnEnable()
@@ -13,27 +17,59 @@ public class MainMenuController : MonoBehaviour
         var root = uiDocument.rootVisualElement;
 
         // 버튼 가져오기
-        startButton = root.Q<Button>("Start");
-        optionsButton = root.Q<Button>("Option");
+        continueButton = root.Q<Button>("Continue");
+        quitBattleButton = root.Q<Button>("QuitBattle");
+        encyclopediaButton = root.Q<Button>("Encyclopedia");
+        statisticsButton = root.Q<Button>("Statistics");
+        settingsButton = root.Q<Button>("Settings");
+        patchNotesButton = root.Q<Button>("PatchNotes");
         quitButton = root.Q<Button>("Exit");
 
         // 버튼 클릭 이벤트 등록
-        startButton.clicked += StartGame;
-        optionsButton.clicked += OpenOptions;
+        continueButton.clicked += ContinueGame;
+        quitBattleButton.clicked += QuitBattle;
+        encyclopediaButton.clicked += OpenEncyclopedia;
+        statisticsButton.clicked += OpenStatistics;
+        settingsButton.clicked += OpenSettings;
+        patchNotesButton.clicked += OpenPatchNotes;
         quitButton.clicked += QuitGame;
     }
 
-    private void StartGame()
+    private void ContinueGame()
     {
-        Debug.Log("게임 시작!");
+        Debug.Log("게임 계속하기!");
         // 씬 로드 예제
         UnityEngine.SceneManagement.SceneManager.LoadScene("BattleScene");
     }
 
-    private void OpenOptions()
+    private void QuitBattle()
     {
-        Debug.Log("옵션 열기");
-        // 옵션 메뉴 구현
+        Debug.Log("전투를 포기합니다.");
+        // 전투 포기 로직 구현
+    }
+
+    private void OpenEncyclopedia()
+    {
+        Debug.Log("백과사전 열기");
+        // 백과사전 메뉴 구현
+    }
+
+    private void OpenStatistics()
+    {
+        Debug.Log("통계 열기");
+        // 통계 메뉴 구현
+    }
+
+    private void OpenSettings()
+    {
+        Debug.Log("설정 열기");
+        // 설정 메뉴 구현
+    }
+
+    private void OpenPatchNotes()
+    {
+        Debug.Log("패치 노트 열기");
+        // 패치 노트 메뉴 구현
     }
 
     private void QuitGame()
